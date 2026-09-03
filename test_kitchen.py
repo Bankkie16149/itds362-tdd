6a925c86e4fd41a9fe7e350be2b040b4da2ea3bc
Tanakorn Tipwarreerattana <tanakorn-tip@Tanakorns-MacBook-Air.local>
2026-09-03 12:08:09 +0700

    RED: add same-unit addition test

--- test_kitchen.py
+++ test_kitchen.py
@@ -20,3 +20,6 @@ def test_quantities_with_same_amount_are_equal():
     assert Quantity(200, "g") == Quantity(200, "g")
 def test_different_units_are_not_equal():
     assert Quantity(1, "oz") != Quantity(1, "g")
+def test_add_same_unit():
+    result = Quantity(200, "g").plus(Quantity(300, "g"))
+    assert result == Quantity(500, "g")
