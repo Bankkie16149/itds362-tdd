a061194d4707e5e9f80113e61d3529a401e4a165
Tanakorn Tipwarreerattana <tanakorn-tip@Tanakorns-MacBook-Air.local>
2026-09-03 11:54:31 +0700

    RED: triangulate with second multiplication case

--- test_kitchen.py
+++ test_kitchen.py
@@ -6,3 +6,8 @@ def test_multiply_quantity():
     result = q.times(3)
     assert result.amount == 600
     assert result.unit == "g"
+def test_multiply_quantity_different_numbers():
+    q = Quantity(10, "g")
+    result = q.times(4)
+    assert result.amount == 40
+    assert result.unit == "g"
